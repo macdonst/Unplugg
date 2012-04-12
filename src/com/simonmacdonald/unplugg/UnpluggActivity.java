7a2ca82614d296964b590c28860f9169811ed0b6
macdonst <simon.macdonald@gmail.com>
2012-04-12 01:04:36 -0400

    Get properly styled time pickers

--- src/com/simonmacdonald/unplugg/UnpluggActivity.java
+++ src/com/simonmacdonald/unplugg/UnpluggActivity.java
@@ -9,13 +9,8 @@ public class UnpluggActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //super.init();
         // super.setIntegerProperty("splashscreen", R.drawable.splash);
-        //this.appView.getSettings().setPluginsEnabled(true);
         this.setStringProperty("errorUrl", "file:///android_asset/www/error.html");
-        //this.appView.getSettings().setUseWideViewPort(true);
-        //this.appView.getSettings().setLoadWithOverviewMode(true);
-        //this.appView.setInitialScale(0); 
         super.loadUrl("file:///android_asset/www/index.html");
     }
 }
